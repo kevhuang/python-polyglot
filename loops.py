9f91f56004e121eb4049c16572431a049021e557
Mark Tausch <tausch86@gmail.com>
2015-04-09 21:30:25 -0700

    found 5 secrets of python

---
+++ loops.py
@@ -0,0 +1,14 @@
+for x in range(1,16):
+  print(x)
+
+for x in [1,2,3,4]:
+  print(x)
+
+for key in {'key': 'val', 'key2': 'val2'}:
+  print(key)
+
+x = 0
+while x < 1 and True:
+  x += 1
+
+print(x)
---
+++ variabls.py
@@ -0,0 +1,18 @@
+x = 100
+y = 1000
+z = [1,2,3,4,5]
+q = True
+s = 'hello world'
+
+
+print(x+y)
+
+print(z)
+
+print(s)
+
+print(s[0:5])
+
+print(s[0:len(s)])
+
+print(s[0:len(s)])
